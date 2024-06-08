416d8ee742889fde5ab2ded18673e4852f37f09a
Rico <ricoarcane23@gmail.com>
2024-06-08 16:21:51 +0700

    adding profile menu

--- app/src/main/java/com/android/example/batikify/screen/home/HomeActivity.kt
+++ app/src/main/java/com/android/example/batikify/screen/home/HomeActivity.kt
@@ -3,10 +3,14 @@ package com.android.example.batikify.screen.home
 import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.android.example.batikify.R
 import com.android.example.batikify.databinding.ActivityHomeBinding
 import com.android.example.batikify.screen.classification.ClassificationActivity
 import com.android.example.batikify.screen.ensiklopedia.EnsiklopediaActivity
+import com.android.example.batikify.screen.profile.ProfileActivity
 
 class HomeActivity: AppCompatActivity(){
 
@@ -24,6 +28,22 @@ class HomeActivity: AppCompatActivity(){
         binding.ensyclopeidaButton.setOnClickListener {
             val intent = Intent(this@HomeActivity,EnsiklopediaActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.option_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_profile -> {
+                val profilePage =  Intent(this@HomeActivity,ProfileActivity::class.java)
+                startActivity(profilePage)
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
