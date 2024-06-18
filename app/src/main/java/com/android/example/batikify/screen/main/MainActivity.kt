5693a4b2bfc63d70cc6e122b637ad4b77139021b
Rico <ricoarcane23@gmail.com>
2024-06-18 23:14:08 +0700

    fixing actionBar in main

--- app/src/main/java/com/android/example/batikify/screen/main/MainActivity.kt
+++ app/src/main/java/com/android/example/batikify/screen/main/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
